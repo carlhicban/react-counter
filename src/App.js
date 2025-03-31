e67e839afa5dfcd35884e8fa6efcfddc829bdb12
carlhicban <68343524+carlhicban@users.noreply.github.com>
2025-04-01 07:54:59 +0800

    Add solution in the test counter date

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const [date, setDate] = useState(new Date().toDateString());
+
+  function addCount() {
+    setCount((d) => d + step);
+    addDate();
+  }
+
+  function addDate() {
+    setDate(() => {
+      const newDate = new Date();
+      newDate.setDate(newDate.getDate() + count);
+      return newDate.toDateString();
+    });
+  }
+
+  function minusCount() {
+    if (count > 0) {
+      setCount((d) => d - step);
+    }
+
+    minusDate();
+  }
+
+  function minusDate() {
+    setDate(() => {
+      const prevDate = new Date(date);
+      prevDate.setDate(prevDate.getDate() - step);
+
+      return prevDate.toDateString();
+    });
+  }
+
+  function addStep() {
+    setStep((s) => s + 1);
+  }
+
+  function minusStep() {
+    setStep((s) => s - 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{ display: "flex" }}>
+        <button onClick={minusStep}>-</button>
+        <p>Step: {step}</p>
+        <button onClick={addStep}>+</button>
+      </div>
+      <div style={{ display: "flex" }}>
+        <button onClick={minusCount}>-</button>
+        <p>Count: {count}</p>
+        <button onClick={addCount}>+</button>
+      </div>
+      <p>
+        {count > 0 ? count + " days from" : ""} today is {date}
+      </p>
     </div>
   );
 }
-
-export default App;
