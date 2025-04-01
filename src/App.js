fdf6c2b9735051c6ae8e54a2b7f01821061b5c99
carlhicban <68343524+carlhicban@users.noreply.github.com>
2025-04-01 08:29:14 +0800

    Update logic in date

--- src/App.js
+++ src/App.js
@@ -3,36 +3,15 @@ import { useState } from "react";
 export default function App() {
   const [step, setStep] = useState(1);
   const [count, setCount] = useState(0);
-  const [date, setDate] = useState(new Date().toDateString());
 
   function addCount() {
     setCount((d) => d + step);
-    addDate();
-  }
-
-  function addDate() {
-    setDate(() => {
-      const newDate = new Date();
-      newDate.setDate(newDate.getDate() + count);
-      return newDate.toDateString();
-    });
   }
 
   function minusCount() {
-    if (count > 0) {
-      setCount((d) => d - step);
-    }
-
-    minusDate();
-  }
-
-  function minusDate() {
-    setDate(() => {
-      const prevDate = new Date(date);
-      prevDate.setDate(prevDate.getDate() - step);
-
-      return prevDate.toDateString();
-    });
+    // if (count > 0) {
+    setCount((d) => d - step);
+    // }
   }
 
   function addStep() {
@@ -42,6 +21,9 @@ export default function App() {
   function minusStep() {
     setStep((s) => s - 1);
   }
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
 
   return (
     <div>
@@ -56,7 +38,14 @@ export default function App() {
         <button onClick={addCount}>+</button>
       </div>
       <p>
-        {count > 0 ? count + " days from" : ""} today is {date}
+        {count === 0
+          ? "Today is "
+          : count > 0
+          ? `${count} day(s) from today is `
+          : count < 0
+          ? `${count} day(s) ago is `
+          : ""}
+        {date.toDateString()}
       </p>
     </div>
   );
